3a9f719b55952d8883e234cfcc692381822811f7
Avaneesh Sathish <avaneesh.sathish@gmail.com>
2022-11-12 14:47:00 -0500

    updated reminder change file paths to your specific file path

--- presentation/preprocessing.py
+++ presentation/preprocessing.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 # read excel file sheets into separate df
-excel_path = '/Users/sheetalsudhir/Documents/BADMBuddies/data/2022_County_Health_Rankings_Data.xlsx'
+excel_path = '/Users/avaneeshs/Documents/BADMBuddies/data/2022_County_Health_Rankings_Data.xlsx'
 df_outcomes_rankings = pd.read_excel(excel_path, "Outcomes & Factors Rankings")
 df_outcomes_subrankings = pd.read_excel(excel_path, "Outcomes & Factors SubRankings")
 df_ranked_measure = pd.read_excel(excel_path, "Ranked Measure Data")
 df_additional_measure = pd.read_excel(excel_path, "Additional Measure Data")
 
 # read csv data into pandas df
-df_analytic_data = pd.read_csv('/Users/sheetalsudhir/Documents/BADMBuddies/data/chr_analytic_data2022.csv')
-df_trends_data = pd.read_csv('/Users/sheetalsudhir/Documents/BADMBuddies/data/chr_trends_csv_2022.csv')
+df_analytic_data = pd.read_csv('/Users/avaneeshs/Documents/BADMBuddies/data/chr_analytic_data2022.csv')
+df_trends_data = pd.read_csv('/Users/avaneeshs/Documents/BADMBuddies/data/chr_trends_csv_2022.csv')
 
 
 def rename_columns(columns, first_row):
