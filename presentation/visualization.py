febffc33c9af047423dd08068526b316b2d44984
Sheetal Sudhir <sheetal01.sudhir@gmail.com>
2022-11-12 20:00:59 -0500

    uodated heatmap

--- presentation/visualization.py
+++ presentation/visualization.py
@@ -6,5 +6,15 @@ import matplotlib.pyplot as plt
 
 df = preprocessing.get_final_df(preprocessing.df_outcomes_rankings, preprocessing.df_outcomes_subrankings, preprocessing.df_ranked_measure, preprocessing.df_additional_measure)
 #display(df)
-map = sns.heatmap(df)
-plt.show()
+#df = preprocessing.get_final_df(preprocessing.df_outcomes_rankings, preprocessing.df_outcomes_subrankings, preprocessing.df_ranked_measure, preprocessing.df_additional_measure)
+#display(df)
+
+fig, ax = plt.subplots(figsize=(84,84))
+corr_mat = df.corr()
+sns.heatmap(corr_mat)
+
+#final_df.corr()
+
+sorted_mat = corr_mat.unstack().sort_values()
+print(sorted_mat)
+
