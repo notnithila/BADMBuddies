ac717530e0ae69bf068c3f9b81b1e68fe04f8515
Avaneesh Sathish <avaneesh.sathish@gmail.com>
2022-11-12 19:21:48 -0500

    seaborn

--- presentation/visualization.py
+++ presentation/visualization.py
@@ -1,8 +1,10 @@
 import preprocessing 
 import seaborn as sns
 import pandas as pd
-from IPython.display import display
-#import matplotlib.pyplot as plt
+#from IPython.display import display
+import matplotlib.pyplot as plt
 
 df = preprocessing.get_final_df(preprocessing.df_outcomes_rankings, preprocessing.df_outcomes_subrankings, preprocessing.df_ranked_measure, preprocessing.df_additional_measure)
-display(df)
+#display(df)
+map = sns.heatmap(df)
+plt.show()
